843f463daec63c2e8cc4e41b78d6487584bd0cbc
thiagodma <thiagodma52@gmail.com>
2019-07-16 17:34:15 -0300

    Nada importante

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import perguntas_respostas_funcoes as prf
 
 stop_words = [prf.limpa_utf8(w) for w in prf.stop_words]
 
-perguntas, respostas = prf.importa_dados()
+perguntasx, respostasx = prf.importa_dados()
