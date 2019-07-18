376790a101f1f470a68a4708819394c8a40807b4
thiagodma <thiagodma52@gmail.com>
2019-07-18 15:15:02 -0300

    Corrigidos problemas com relacao às stopwords e adicionado o código para fazer a clusterização

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 import perguntas_respostas_funcoes as prf
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from scipy.cluster import hierarchy
 
-
-#As stop_words vem com acentos/cedilhas. Aqui eu tiro os caracteres indesejados
-stop_words = [prf.limpa_utf8(w) for w in prf.stop_words]
+# stop-words
+stop_words = prf.define_stop_words()
 
 #Importa os dados e os retorna já tratados
-perguntasx, respostasx = prf.importa_dados()
+perguntas, respostas = prf.importa_dados(stop_words)
+
+#Define como saber qual pergunta corresponde a qual resposta quando olhando o csv
+perguntas_respostas_id = []
+
+for i in range(len(perguntas)):
+    perguntas_respostas_id.append('P' + str(i))
+
+for i in range(len(respostas)):
+    perguntas_respostas_id.append('R' + str(i))
 
 
+#Concatena as duas listas
+textos = perguntas + respostas
+
+#Vetorizando e aplicando o tfidf
+vec = CountVectorizer()
+bag_palavras = vec.fit_transform(textos)
+feature_names = vec.get_feature_names()
+base_tfidf = TfidfTransformer().fit_transform(bag_palavras)
+base_tfidf = base_tfidf.todense()
+
+#Clustering
+print('Começou a clusterização.\n')
+t = time.time()
+clusters_por_cosseno = hierarchy.linkage(base_tfidf,"average", metric="cosine") #pode testar metric="euclidean" também
+plt.figure()
+dn = hierarchy.dendrogram(clusters_por_cosseno)
+elpsd = time.time() - t
+print('Tempo para fazer a clusterização: ' + str(elpsd) + '\n')
+
+# Separa a que Cluster pertence cada texto, pela ordem na lista de textos,
+# dado o parâmetro de limite de dissimilaridade threshold
+limite_dissimilaridade = 0.85
+id_clusters = hierarchy.fcluster(clusters_por_cosseno, limite_dissimilaridade, criterion="distance")
+
+#Tentando visualizar os dados e vendo o número de amostras por cluster
+analise = prf.analisa_clusters(base_tfidf, id_clusters)
+
+#Colocando em dataframes
+X = pd.DataFrame(id_clusters,columns=['cluster_id'])
+Y = pd.DataFrame(perguntas_respostas_id ,columns=['perguntas_respostas'])
+Z = X.join(Y)
+
+print('Foram encontradas ' + str(max(Z['cluster_id'])) + ' clusters\n')
+
+#Exporta as tabelas
+Z.to_csv('cluster_perguntas_respostas_cosseno.csv', sep='|', 
+                    index=False, encoding='utf-8')
--- perguntas_respostas_funcoes.py
+++ perguntas_respostas_funcoes.py
@@ -1,17 +1,13 @@
 import pandas as pd
-import re, unicodedata, nltk, time, itertools
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import re, unicodedata, time, itertools, nltk
+from sklearn.decomposition import TruncatedSVD
 from stop_words import get_stop_words
 nltk.download('stopwords')
-
 #==============================================================================
 #Definindo algumas variáveis úteis
-
-# stop-words
-stop_words = get_stop_words('portuguese')
-stop_words = stop_words + nltk.corpus.stopwords.words('portuguese')
-stop_words = stop_words + ['art','dou','secao','pag','pagina', 'in', 'inc', 'obs', 'sob', 'cnpj', 'ltda']
-stop_words = stop_words + ['ndash', 'mdash', 'lsquo','rsquo','ldquo','rdquo','bull','hellip','prime','lsaquo','rsaquo','frasl',]
-stop_words = list(dict.fromkeys(stop_words))
 
 #todas as combinacoes possíveis para palavras que indicam comeco de resposta sem ser do E-SIC
 words = ['atencao','resposta','relacao','atendimento','contato','solicitacao','questionamento','consulta','preenchimento','questionamentos']
@@ -20,7 +16,21 @@ d = [' '.join(comb) for comb in c]
 combinacoes = '|'.join(d)
 #==============================================================================
 
-def importa_dados():
+#Pega stop_words e as trata para ficarem no formato correto
+def define_stop_words():
+    
+    stop_words = get_stop_words('portuguese')
+    stop_words = stop_words + nltk.corpus.stopwords.words('portuguese')
+    stop_words = stop_words + ['art','dou','secao','pag','pagina', 'in', 'inc', 'obs', 'sob', 'cnpj', 'ltda']
+    stop_words = stop_words + ['ndash', 'mdash', 'lsquo','rsquo','ldquo','rdquo','bull','hellip','prime','lsaquo','rsaquo','frasl', 'ordm']
+    stop_words = list(dict.fromkeys(stop_words))
+    stop_words = ' '.join(stop_words)
+    #As stop_words vem com acentos/cedilhas. Aqui eu tiro os caracteres indesejados
+    stop_words = limpa_utf8(stop_words)
+    
+    return stop_words
+
+def importa_dados(stop_words):
     
     print('\nComeçou a importação dos dados.\n')
     
@@ -31,7 +41,7 @@ def importa_dados():
     respostas = df['Histórico'].values.tolist()
     
     t = time.time()
-    perguntas_out = [trata_perguntas(pergunta) for pergunta in perguntas]
+    perguntas_out = [trata_perguntas(pergunta, stop_words) for pergunta in perguntas]
     elpsd = time.time() - t
     print('Tempo para processar as perguntas: ' + str(elpsd) + '\n')
     
@@ -39,16 +49,16 @@ def importa_dados():
     respostas_out = []
     for resposta in respostas:
         if(re.search('e-sic|E-SIC',resposta) == None):
-            respostas_out.append(trata_respostas_1(resposta))
+            respostas_out.append(trata_respostas_1(resposta,stop_words))
         else:
-            respostas_out.append(trata_respostas_2(resposta))
+            respostas_out.append(trata_respostas_2(resposta, stop_words))
     elpsd = time.time() - t
     print('Tempo para processar as respostas: ' + str(elpsd) + '\n')
     
     return perguntas_out, respostas_out
 
 #Coloca as perguntas no melhor formato para convertê-las para um BOW
-def trata_perguntas(texto):
+def trata_perguntas(texto, stop_words):
     
     #converte todos caracteres para letra minúscula
     texto_lower = texto.lower()
@@ -60,14 +70,16 @@ def trata_perguntas(texto):
     #Remove acentos e pontuação
     texto_sem_acento_pontuacao = limpa_utf8(texto_sem_sites)
     
+    #Retira numeros romanos e stopwords
+    texto_sem_acento_pontuacao = texto_sem_acento_pontuacao.split()
+    texto_sem_stopwords = [roman2num(palavra,stop_words) for palavra in texto_sem_acento_pontuacao]
+    texto_sem_stopwords = ' '.join(texto_sem_stopwords)
+    
     #Remove hifens e barras
-    texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_acento_pontuacao)
+    texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_stopwords)
     
     #Troca qualquer tipo de espacamento por espaço
     texto_sem_espacamentos = re.sub(r'\s', ' ', texto_sem_hifens_e_barras)
-    
-    #Remove dígitos
-    #texto_sem_digitos = re.sub(r'\d','', texto_sem_espacamentos)
     
     #Remove pontuacao e digitos
     texto_sem_pontuacao_digitos = re.sub('[^A-Za-z]', ' ' , texto_sem_espacamentos)
@@ -78,24 +90,22 @@ def trata_perguntas(texto):
     #Tira despedida
     texto_sem_despedida = re.sub(r'(obrigad|atenciosamente|desde ja).*', '', texto_sem_cabecalho)
     
-    #Remove espaços extras
-    texto_sem_espacos_extras = re.sub(' +', ' ', texto_sem_despedida)
-    
-    #Retira stopwords que possam ter reaparecido
-    texto_aux = texto_sem_espacos_extras.split()
-    texto_limpo = []
-    for palavra in texto_aux:
-        if (palavra in stop_words) or (len(palavra)==1): 
-            texto_limpo.append('')
-        else:
-            texto_limpo.append(palavra)
+    #Retira stopwords que possam ter reaparecido e numeros romanos
+    texto_sem_despedida = texto_sem_despedida.split()
+    texto_limpo = [roman2num(palavra, stop_words) for palavra in texto_sem_despedida]
     
     texto_limpo = ' '.join(texto_limpo)
+    
+    #Remove dígitos
+    texto_limpo = re.sub(r'\d','', texto_limpo)
+    
+    #Remove espaços extras
+    texto_limpo = re.sub(' +', ' ', texto_limpo)    
     
     return texto_limpo
 
 #Coloca as respostas no melhor formato para convertê-las para um BOW. Retorna os índices das respostas que não estão finalizadas
-def trata_respostas_1(texto):
+def trata_respostas_1(texto,stop_words):
     
     #converte todos caracteres para letra minúscula
     texto_lower = texto.lower()
@@ -107,8 +117,13 @@ def trata_respostas_1(texto):
     #Remove acentos e pontuação
     texto_sem_acento_pontuacao = limpa_utf8(texto_sem_sites)
     
+    #Retira numeros romanos e stopwords
+    texto_sem_acento_pontuacao = texto_sem_acento_pontuacao.split()
+    texto_sem_stopwords = [roman2num(palavra, stop_words) for palavra in texto_sem_acento_pontuacao]
+    texto_sem_stopwords = ' '.join(texto_sem_stopwords)
+    
     #Remove hifens e barras
-    texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_acento_pontuacao)
+    texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_stopwords)
     
     #Troca qualquer tipo de espacamento por espaço
     texto_sem_espacamentos = re.sub(r'\s', ' ', texto_sem_hifens_e_barras)
@@ -120,7 +135,7 @@ def trata_respostas_1(texto):
     texto_sem_pontuacao = re.sub('[^A-Za-z]', ' ' , texto_sem_digitos)
     
     #Remove espaços extras
-    texto_sem_espacos_extras = re.sub(' +', ' ', texto_sem_pontuacao)    
+    texto_sem_espacos_extras = re.sub(' +', ' ', texto_sem_pontuacao) 
     
     #Pega apenas a resposta final (nem sempre o padrao eh bonitinho)
     firstRegex = r'(' + combinacoes + ') (.+?) (favor avalie resposta|$)'
@@ -135,7 +150,7 @@ def trata_respostas_1(texto):
     
     #Retira stopwords que possam ter reaparecido e numeros romanos
     texto_so_resposta_final = texto_so_resposta_final.split()
-    texto_limpo = [roman2num(palavra) for palavra in texto_so_resposta_final if palavra not in stop_words]
+    texto_limpo = [roman2num(palavra, stop_words) for palavra in texto_so_resposta_final]
     
     texto_limpo = ' '.join(texto_limpo)
     
@@ -151,7 +166,7 @@ def trata_respostas_1(texto):
 
 
 #Coloca as respostas no melhor formato para convertê-las para um BOW. Essa função é específica para as perguntas com E-SIC
-def trata_respostas_2(texto):
+def trata_respostas_2(texto, stop_words):
     
     #converte todos caracteres para letra minúscula
     texto_lower = texto.lower()
@@ -163,8 +178,13 @@ def trata_respostas_2(texto):
     #Remove acentos e pontuação
     texto_sem_acento_pontuacao = limpa_utf8(texto_sem_sites)
     
+    #Retira stopwords que possam ter reaparecido e numeros romanos
+    texto_sem_acento_pontuacao = texto_sem_acento_pontuacao.split()
+    texto_sem_stopwords = [roman2num(palavra, stop_words) for palavra in texto_sem_acento_pontuacao if palavra not in stop_words]
+    texto_sem_stopwords = ' '.join(texto_sem_stopwords)
+    
     #Remove hifens e barras
-    texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_acento_pontuacao)
+    texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_stopwords)
     
     #Troca qualquer tipo de espacamento por espaço
     texto_sem_espacamentos = re.sub(r'\s', ' ', texto_sem_hifens_e_barras)
@@ -192,8 +212,7 @@ def trata_respostas_2(texto):
     
     #Retira stopwords que possam ter reaparecido e numeros romanos
     texto_so_resposta_final = texto_so_resposta_final.split()
-    texto_limpo = [roman2num(palavra) for palavra in texto_so_resposta_final if palavra not in stop_words]
-    
+    texto_limpo = [roman2num(palavra, stop_words) for palavra in texto_so_resposta_final if palavra not in stop_words]
     texto_limpo = ' '.join(texto_limpo)
     
     #Remove dígitos
@@ -215,19 +234,19 @@ def limpa_utf8(texto):
         # Unicode normalize transforma um caracter em seu equivalente em latin.
         nfkd = unicodedata.normalize('NFKD', palavra)
         palavra_sem_acento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
-    
-        #remove stopwords
-        if palavra_sem_acento in stop_words: 
-            texto_tratado.append(' ')
-        else:
-            texto_tratado.append(palavra_sem_acento)
+        texto_tratado.append(palavra_sem_acento)
             
     return ' '.join(texto_tratado)
 
 
-def roman2num(roman, values={'m': 1000, 'd': 500, 'c': 100, 'l': 50,
+def roman2num(roman, stop_words, values={'m': 1000, 'd': 500, 'c': 100, 'l': 50,
                                 'x': 10, 'v': 5, 'i': 1}):
     roman = limpa_utf8(roman)
+    
+    #remove stopwords
+    if roman in stop_words: 
+        return ''
+      
 
     #como eu vou tirar numeros de qualquer forma, posso simplesmente retornar um numero
     if(len(roman) < 2 ):
@@ -251,8 +270,40 @@ def roman2num(roman, values={'m': 1000, 'd': 500, 'c': 100, 'l': 50,
         return str(total + num2)
     else:
         return str(numbers[0])
+
+#Reduz a dimensionalidade dos dados
+def SVD(dim,base_tfidf):
+    svd = TruncatedSVD(n_components = dim, random_state = 42)
+    base_tfidf_reduced = svd.fit_transform(base_tfidf)
+    print('\nNúmero de dimensoes de entrada: ' + str(base_tfidf.shape[1]))
+    print(str(dim) + ' dimensões explicam ' + str(svd.explained_variance_ratio_.sum()) + ' da variância.\n' )
+    return base_tfidf_reduced
                 
-        
+
+#Visualiza as cluster definidas pelo algoritmo. Além disso também retorna o número
+#de normas por cluster.
+def analisa_clusters(base_tfidf, id_clusters):
+    
+    clusters = np.unique(id_clusters)
+    
+    #inicializa o output da funcao
+    n_normas = np.zeros(len(clusters)) #numero de normas pertencentes a uma cluster
+    
+    #reduz a dimensionalidade para 2 dimensoes
+    base_tfidf_reduced = SVD(2,base_tfidf)
+    X = base_tfidf_reduced[:,0]
+    Y = base_tfidf_reduced[:,1]
+    
+    colors = cm.rainbow(np.linspace(0, 1, len(n_normas)))
+
+    for cluster, color in zip(clusters, colors):
+        idxs = np.where(id_clusters == cluster) #a primeira cluster não é a 0 e sim a 1
+        n_normas[cluster-1] = len(idxs[0])
+        x = X[idxs[0]]
+        y = Y[idxs[0]]
+        plt.scatter(x, y, color=color)
+    
+    return n_normas 
     
         
         
