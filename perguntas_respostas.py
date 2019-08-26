d1d55d5dd8e34fca82034c23d5ebe8ff38b90782
Thiago Dantas <thiagodma52@gmail.com>
2019-08-26 08:24:19 -0300

    Criei a classe das respostas. Preciso tratar os textos ainda

--- perguntas_respostas.py
+++ perguntas_respostas.py
@@ -91,3 +91,59 @@ class Perguntas(ClassicClustering):
         textos_por_cluster_perguntas = pd.DataFrame(lista_textos_por_cluster_perguntas,
                                        columns=['textos_id','cluster_id','texto','texto_tratado'])
         textos_por_cluster_perguntas.to_csv('textos_por_cluster_perguntas.csv', sep='|', index=False, encoding='utf-8')
+
+class Respostas(ClassicClustering):
+
+    def __init__(self):
+        ClassicClustering.__init__(self)
+
+    def importa_textos(self):
+
+        print('\nComeçou a importação dos textos.')
+
+        #primeiramente importa os dados de forma bruta
+        df = pd.read_excel('GGALI__ago2018_a_mai2019.xlsx', sheet_name='Protocolos')
+        df = df[['Pergunta','Histórico']]
+        self.textos = df['Histórico'].values.tolist()
+
+        t = time.time()
+        self.textos_tratados = [self.trata_textos(resposta) for resposta in self.textos]
+        self.textos_id = ['R' + str(i) for i in range(len(self.textos_tratados))]
+        elpsd = time.time() - t
+        print('Tempo para processar os textos: ' + str(elpsd))
+
+    def trata_textos(self, texto):
+
+        converte todos caracteres para letra minúscula
+        texto_lower = texto.lower()
+        texto_lower = re.sub(' +', ' ', texto_lower)
+
+        #tira sites
+        texto_sem_sites =  re.sub('(http|www)[^ ]+','',texto_lower)
+
+        #Remove acentos e pontuação
+        texto_sem_acento_pontuacao = self.limpa_utf8(texto_sem_sites)
+
+        #Retira numeros romanos e stopwords
+        texto_sem_acento_pontuacao = texto_sem_acento_pontuacao.split()
+        texto_sem_stopwords = [self.tira_stopwords_e_romanos(palavra) for palavra in texto_sem_acento_pontuacao]
+        texto_sem_stopwords = ' '.join(texto_sem_stopwords)
+
+        #Remove hifens e barras
+        texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_stopwords)
+
+        #Troca qualquer tipo de espacamento por espaço
+        texto_sem_espacamentos = re.sub(r'\s', ' ', texto_sem_hifens_e_barras)
+
+        #Remove pontuacao e digitos
+        texto_sem_pontuacao_digitos = re.sub('[^A-Za-z]', ' ' , texto_sem_espacamentos)
+
+    def generate_csvs(self, info_cluster, id_clusters):
+
+        #cria o arquivo info_cluster_respostas.csv
+        info_cluster.to_csv('info_cluster_respostas.csv', sep='|', index=False, encoding='utf-8')
+        lista_textos_por_cluster_respostas = list(zip(self.textos_id, id_clusters, self.textos, self.textos_tratados))
+        #cria o arquivo textos_por_cluster_perguntas.csv
+        textos_por_cluster_respostas = pd.DataFrame(lista_textos_por_cluster_respostas,
+                                       columns=['textos_id','cluster_id','texto','texto_tratado'])
+        textos_por_cluster_respostas.to_csv('textos_por_cluster_respostas.csv', sep='|', index=False, encoding='utf-8')
