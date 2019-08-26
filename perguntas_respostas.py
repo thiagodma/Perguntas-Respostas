d15dbc6b8d3c5b9b08be5ae1ee4f2dc4eabd40ee
Thiago Dantas <thiagodma52@gmail.com>
2019-08-26 12:11:47 -0300

    Melhorei o processamento das respostas. Preciso ver a resposta 989.

--- perguntas_respostas.py
+++ perguntas_respostas.py
@@ -114,29 +114,61 @@ class Respostas(ClassicClustering):
 
     def trata_textos(self, texto):
 
-        converte todos caracteres para letra minúscula
+        #converte todos caracteres para letra minúscula
         texto_lower = texto.lower()
         texto_lower = re.sub(' +', ' ', texto_lower)
 
+        isEsic = re.search('e-sic',texto_lower) != None
+
+        if(not isEsic):
+            #retira todo o lixo antes de começar a resposta (not e-sic)
+            texto_sem_cabecalho = texto_lower
+            while True:
+                m = re.search(r'(data/hora: \d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2})(.*)', texto_sem_cabecalho)
+                if m==None: break
+                texto_sem_cabecalho = m.group(2)
+        else:
+            #retira todo o lixo antes de começar a resposta (e-sic)
+            m = re.search(r'(acesso\s+concedido)(.*)',texto_lower)
+            if m==None: return 'não há resposta para essa pergunta'
+            texto_sem_cabecalho = m.group(2)
+
+        #tira a saudacao (all)
+        texto_sem_saudacao = re.sub(r'prezado\s+\(a\)\s+senhor\s+\(a\),.*?,','',texto_sem_cabecalho)
+
+        #retira a despedida (not e-sic)
+        texto_sem_despedida = re.sub(r'por\s+favor,\s+avalie\s+a\s+resposta.*','',texto_sem_saudacao)
+
+        #retira a despedida (e-sic)
+        if(isEsic):
+            texto_sem_despedida = re.sub(r'responsável:.*','',texto_sem_despedida)
+
+        #remove despedida geral
+        texto_sem_despedida = re.sub(r'atenciosamente.*','',texto_sem_despedida)
+
         #tira sites
-        texto_sem_sites =  re.sub('(http|www)[^ ]+','',texto_lower)
+        texto_sem_sites =  re.sub('(http|www)[^ ]+','',texto_sem_despedida)
 
         #Remove acentos e pontuação
         texto_sem_acento_pontuacao = self.limpa_utf8(texto_sem_sites)
 
-        #Retira numeros romanos e stopwords
-        texto_sem_acento_pontuacao = texto_sem_acento_pontuacao.split()
-        texto_sem_stopwords = [self.tira_stopwords_e_romanos(palavra) for palavra in texto_sem_acento_pontuacao]
-        texto_sem_stopwords = ' '.join(texto_sem_stopwords)
-
         #Remove hifens e barras
-        texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_stopwords)
+        texto_sem_hifens_e_barras = re.sub('[-\/]', ' ', texto_sem_acento_pontuacao)
 
         #Troca qualquer tipo de espacamento por espaço
         texto_sem_espacamentos = re.sub(r'\s', ' ', texto_sem_hifens_e_barras)
 
         #Remove pontuacao e digitos
         texto_sem_pontuacao_digitos = re.sub('[^A-Za-z]', ' ' , texto_sem_espacamentos)
+
+        #Retira numeros romanos e stopwords
+        texto_sem_pontuacao_digitos = texto_sem_pontuacao_digitos.split()
+        texto_sem_stopwords = [self.tira_stopwords_e_romanos(palavra) for palavra in texto_sem_pontuacao_digitos]
+        texto_sem_stopwords = ' '.join(texto_sem_stopwords)
+
+        texto_limpo = re.sub(' +', ' ', texto_sem_stopwords)
+
+        return texto_limpo
 
     def generate_csvs(self, info_cluster, id_clusters):
 
