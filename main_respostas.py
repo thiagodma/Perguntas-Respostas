ae2b8e4df0ca1a4f4a8731615abd8eecccd1e770
Thiago Dantas <thiagodma52@gmail.com>
2019-08-28 08:10:27 -0300

    Adicionei o stemming

--- main_respostas.py
+++ main_respostas.py
@@ -6,24 +6,29 @@ stopwords = ['prezado', 'prezados', 'prezada', 'prezadas', 'gereg', 'ggali',
              'usuario', 'usuaria', 'deseja','gostaria', 'boa tarde', 'bom dia',
              'boa noite', 'rdc', 'ins', 'geare', 'resposta', 'link']
 
+#inicializa o atributo de stopwords
 respostas.define_stop_words(user_defined_stopwords=stopwords)
 
+#carrega os textos e os trata
 respostas.importa_textos()
-import pdb; pdb.set_trace()
+
+#faz o stemming nos textos
+respostas.stem()
+
 #vetoriza e aplica o tfidf
 base_tfidf = respostas.vec_tfidf()
 
 #reduzindo a dimensionalidade
 dim = 2000
 base_tfidf_reduced = respostas.SVD(base_tfidf,dim=dim)
-
+import pdb; pdb.set_trace()
 #Clustering
 print('Começou a clusterização.')
 t = time.time()
 clusters_por_cosseno = hierarchy.linkage(base_tfidf_reduced,"average", metric="cosine") #pode testar metric="euclidean" também
 plt.figure()
 dn = hierarchy.dendrogram(clusters_por_cosseno)
-plt.savefig('dendrogram.png')
+plt.savefig('dendrogram_respostas.png')
 elpsd = time.time() - t
 print('Tempo para fazer a clusterização: ' + str(elpsd) + '\n')
 
